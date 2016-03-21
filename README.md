f060ce185e847dbc0c89f36023bb946335d4d596
MilenaStevanovic <milena.stevanovic@mdprofy.com>
2016-03-21 18:05:15 +0100

    script cmpleted

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Getting-and-Cleaning-Data-Course-Project
+
+Script run_analysis.R does the following:
+- Read all available .txt files (get from "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
+UCI HAR Dataset/test/X_test.txt 		-> variable_test
+UCI HAR Dataset/test/Y_test.txt 		-> activity_test 
+UCI HAR Dataset/train/X_train.txt 		-> variable_train
+UCI HAR Dataset/train/Y_train.txt		-> activity_train
+UCI HAR Dataset/test/subject_test.txt	-> subject_test 
+UCI HAR Dataset/train/subject_train.txt	-> subject_train
+UCI HAR Dataset/features.txt			-> features
+UCI HAR Dataset/activity_labels.txt		-> activity_labels
+
+- Extracts only the measurements on the mean and standard deviation for each measurement.
+variable_test and variable_train take only columns regarding mean and standard deviation
+features data set contains full list of measurements
+Details of measurements are given in source zipped data (in UCI HAR Dataset/features_info.txt)
+
+- Merges the training and the test sets to create one data set.
+variable_test is bound with subject_test and activity_test creating test data set
+variable_train is bound with subject_train and activity_train creating train data set
+test data set and train data set are bound in one data set (named full)
+
+- Appropriately labels the data set with descriptive variable names.
+descriptive variable names are get from features (only regarding mean and standard deviation)
+
+- Uses descriptive activity names to name the activities in the data set
+descriptive activity names are get from activity_labels
+full data set is merged with activity_labels
+
+- From the full data set creates a second, independent tidy data set with the average of each variable for each activity and each subject
+gather function takes "variable" columns and collapses into key-value pairs, duplicating all other columns
+group_by function gropus by activity,subject and variable
+summarize function returns mean value for grouped data 
+tidy data set is named DS_avg
+DS_avg is written in tidy.txt file as result of the function
---
+++ run_analysis.R
@@ -0,0 +1,42 @@
+setwd("C:/Projects/R/Coursera")
+library(dplyr)
+library(tidyr)
+
+#Read all .txt files
+variable_test <- read.table("./UCI HAR Dataset/test/X_test.txt",header = FALSE)
+activity_test <- read.table("./UCI HAR Dataset/test/Y_test.txt",header = FALSE)
+variable_train <- read.table("./UCI HAR Dataset/train/X_train.txt",header = FALSE)
+activity_train <- read.table("./UCI HAR Dataset/train/Y_train.txt",header = FALSE)
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt",header = FALSE)
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt",header = FALSE)
+features <- read.table("./UCI HAR Dataset/features.txt",header = FALSE)
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt",header = FALSE)
+
+
+#2.Extracts only the measurements on the mean and standard deviation for each measurement.
+features[,2]<-as.character(features[,2])
+features_msd_rows<-features[grepl("*mean()|std()*",features$V2)&!grepl("meanFreq()",features$V2),1]
+variable_test<-variable_test[,features_msd_rows]
+variable_train<-variable_train[,features_msd_rows]
+
+#1.Merges the training and the test sets to create one data set.
+test<-cbind(variable_test, subject_test, activity_test)
+train<-cbind(variable_train, subject_train, activity_train)
+full<-rbind(test,train)
+
+#4.Appropriately labels the data set with descriptive variable names.
+features_msd<-features[grepl("*mean()|std()*",features$V2)&!grepl("meanFreq()",features$V2),2]
+names(full)<-c(features_msd,"subject","Act")
+
+#3.Uses descriptive activity names to name the activities in the data set
+activity_labels[,2]<-as.character(activity_labels[,2])
+names(activity_labels)[2]<-"activity"
+full<-full %>% merge(activity_labels,by.x="Act",by.y="V1",all=TRUE,sort = FALSE) %>% select(-1)
+
+#5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+DS_avg <- full %>% 
+    gather(variable, value, 1:(ncol(full)-2), na.rm= TRUE) %>%
+    group_by(activity,subject,variable) %>%
+    summarize(mean = mean(value)) #%>% head()
+
+write.table(DS_avg, "./tidy.txt", row.name = FALSE, quote = FALSE)
